8725bdb5a5f8a062a26cde94cf7116e52d4dd92c
roydamaizalukhu <114473606+roydamaizalukhu@users.noreply.github.com>
2024-03-15 23:23:32 +0700

    Add files via upload

---
+++ penjelasan.java
@@ -0,0 +1,10 @@
+//penjelasan
+
+Kode ini merupakan kode program Java yang membuat objek dari kelas RightTriangle dan Triangle.
+Pada baris ke-3, objek ABC dari kelas RightTriangle dibuat dengan memanggil constructor dengan parameter name, base, dan height. Nama objek adalah "ABC", nilai base adalah 3.0, dan nilai height adalah 4.0.
+Pada baris ke-4, objek BCD dari kelas RightTriangle dibuat dengan memanggil constructor dengan parameter name, base, dan height. Nama objek adalah "BCD", nilai base adalah 3.0, dan nilai height adalah 4.0.
+Pada baris ke-5, objek CDE dari kelas Triangle dibuat dengan memanggil constructor dengan parameter name, side1, side2, dan side3. Nama objek adalah "CDE", nilai side1 adalah 3.0, nilai side2 adalah 4.0, dan nilai side3 adalah 5.0.
+Pada baris ke-6, fungsi getInfo() dari objek ABC dipanggil untuk mendapatkan informasi tentang objek tersebut.
+Pada baris ke-7, fungsi getInfo() dari objek BCD dipanggil untuk mendapatkan informasi tentang objek tersebut.
+Pada baris ke-8, fungsi getInfo() dari objek CDE dipanggil untuk mendapatkan informasi tentang objek tersebut.
+Pada kode ini, objek ABC dan BCD dibuat dari kelas RightTriangle, sedangkan objek CDE dibuat dari kelas Triangle. Fungsi getInfo() dipanggil untuk mendapatkan informasi tentang setiap objek.
